e3c8085d1a46e863845f5bfd663772b2ef248cf0
PascalUlor <pascalulor@yahoo.com>
2020-07-29 02:08:30 +0100

    API

--- src/util/index.js
+++ src/util/index.js
@@ -1,16 +1,22 @@
-const serverType = {CPU: 2, RAM: 32, HDD: 100}
-const virtualMachines = [{CPU: 1, RAM: 16, HDD: 10}, {CPU: 1, RAM: 16, HDD: 10}, {CPU: 2, RAM: 32, HDD: 100}]
-
-
+/**
+ * +calculate(serverType: Server, virtualMachines: VirtualMachine[1..*]): int
+ *
+ * @param {*} serverType
+ * @param {*} virtualMachines
+ * @returns int
+ */
 const calculate = (serverType, virtualMachines) => {
     let count = 0;
 
     while (virtualMachines.length > 0){
-        let item = virtualMachines.shift();
+        let vM = virtualMachines.shift();
+
+        console.log(serverType)
         
-        if(item['CPU'] < serverType['CPU'] || item['RAM'] < serverType['RAM'] && serverType['HDD'] % item['HDD'] === 0){
-            serverType['CPU'] - item['CPU']
-            serverType['RAM'] - item['RAM']
+        if(vM['CPU'] < serverType['CPU'] && vM['RAM'] <= serverType['RAM'] && vM['HDD'] <= serverType['HDD']){
+            serverType['CPU'] -= vM['CPU'];
+            serverType['RAM'] -= vM['RAM'];
+            serverType['HDD'] -= vM['HDD'];
             count++;
         }
     }
